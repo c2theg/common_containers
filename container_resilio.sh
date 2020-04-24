cdae305c8f6167e8cbcb9cddd05a2d98521080d7
Chris Gray <christophermjgray@gmail.com>
2020-04-24 12:36:37 -0400

    Update container_resilio.sh

--- container_resilio.sh
+++ container_resilio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #clear
-# Version: 0.0.2
+# Version: 0.0.3
 # Updated: 4/24/2020
 #---------------------------
 # https://hub.docker.com/r/resilio/sync
@@ -17,9 +17,11 @@ WEBUI_PORT=8888
 mkdir -p $INSTALL_DIR
 mkdir -p $DATA_DIR
 
+#docker volume create $DATA_DIR
+
 docker run -d --name Sync \
            -p 0.0.0.0:$WEBUI_PORT:8888 \
            -v $INSTALL_DIR:/mnt/sync \
-           -v $DATA_DIR:$DATA_DIR \
+           --mount source=$DATA_DIR,target=/mnt/mounted_folders/sync/ \
            --restart on-failure \
            resilio/sync
