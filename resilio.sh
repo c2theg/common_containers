62a41835dcf9bcc7bfebcf23554c12f521c79faa
Chris Gray <christophermjgray@gmail.com>
2020-04-24 11:19:05 -0400

    Update resilio.sh

--- resilio.sh
+++ resilio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #clear
-# Version: 0.0.1
+# Version: 0.0.2
 # Updated: 4/24/2020
 #---------------------------
 # https://hub.docker.com/r/resilio/sync
@@ -16,7 +16,6 @@ mkdir -p $DATA_FOLDER
 
 docker run -d --name Sync \
            -p 127.0.0.1:$WEBUI_PORT:8888 \
-           -p 55555 \
            -v $DATA_FOLDER:/mnt/sync \
            --restart on-failure \
            resilio/sync
